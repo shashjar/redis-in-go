496f66ec0b893e6e245eb093150c5223f62353df
Shashank Jarmale <shashankjarmale1@gmail.com>
2024-09-19 23:16:31 -0400

    Moved functionality for handling a specific connection (reading/writing data) from server.go to socket.go. Keep server.go simple - just any configuration (such as for the logger) and run the server itself.

--- app/server.go
+++ app/server.go
@@ -1,59 +1,21 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"log"
 	"net"
 	"os"
-	"strings"
 )
 
-func readCommand(conn net.Conn) string {
-	message, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading data from connection:", err.Error())
-		os.Exit(1)
-	}
-	message = strings.TrimSuffix(message, "\n")
-	fmt.Printf("Read command: %s\n", message)
-	return message
+func configureLogger() {
+	log.SetFlags(0)
 }
 
-// TODO: right now just uses \n at end --> implement actual Redis protocol
-// TODO: instead of hard-coding these as an if-else block, can use a map of string to function
-func writeResponse(message string, conn net.Conn) {
-	if strings.ToLower(message) == "ping" {
-		_, err := fmt.Fprintf(conn, "+%s\r\n", []byte("PONG"))
-		if err != nil {
-			fmt.Println("Error writing PONG to connection: ", err.Error())
-			os.Exit(1)
-		}
-	} else if strings.ToLower(message[:4]) == "echo" {
-		_, err := fmt.Fprintf(conn, "%s\r\n", []byte(message[5:]))
-		if err != nil {
-			fmt.Println("Error executing ECHO on connection:", err.Error())
-			os.Exit(1)
-		}
-	} else {
-		fmt.Printf("Unknown command: %s\n", message)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	fmt.Println("Handling connection")
-	for {
-		message := readCommand(conn)
-		writeResponse(message, conn)
-	}
-}
-
-func main() {
-	fmt.Println("Running server...")
+func runServer() {
+	log.Println("Running server...")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		log.Println("Failed to bind to port 6379:", err.Error())
 		os.Exit(1)
 	}
 	defer l.Close()
@@ -61,10 +23,15 @@ func main() {
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection:", err.Error())
-			os.Exit(1)
+			log.Println("Error accepting connection:", err.Error())
+			continue
 		}
 
 		go handleConnection(conn)
 	}
+}
+
+func main() {
+	configureLogger()
+	runServer()
 }
---
+++ app/socket.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	log.Println("Handling connection")
+	for {
+		command, err := readCommand(conn)
+		log.Println("Read command:", command, err)
+		if err != nil {
+			log.Println("Error reading command:", err.Error())
+			os.Exit(1)
+		}
+
+		if len(command) > 0 {
+			executeCommand(command, conn)
+		}
+	}
+}
+
+func readCommand(conn net.Conn) ([]string, error) {
+	buf := make([]byte, 128)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if n == 0 {
+		return []string{}, nil
+	}
+
+	buf = buf[:n]
+
+	cmd, err := parseCommand(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return cmd, nil
+}
+
+// TODO: instead of hard-coding these as an if-else block, can use a map of string to function
+// TODO: probably want to move this function to a different file eventually (specifically for executing the Redis commands once they've been parsed)
+func executeCommand(command []string, conn net.Conn) {
+	switch strings.ToLower(command[0]) {
+	case "ping":
+		_, err := conn.Write([]byte(toSimpleString("PONG")))
+		if err != nil {
+			log.Println("Error writing PONG to connection: ", err.Error())
+			os.Exit(1)
+		}
+	case "echo":
+		_, err := conn.Write([]byte(toBulkString(command[1])))
+		if err != nil {
+			log.Println("Error executing ECHO on connection:", err.Error())
+			os.Exit(1)
+		}
+	case "command": // TODO: assumes this is COMMAND DOCS and returns an empty array to get redis-cli to work
+		_, err := conn.Write([]byte("*0\r\n"))
+		if err != nil {
+			log.Println("Error executing COMMAND DOCS on connection:", err.Error())
+			os.Exit(1)
+		}
+	default:
+		log.Printf("Unknown command: %s\n", command)
+		_, err := conn.Write([]byte(toSimpleString("Unknown command")))
+		if err != nil {
+			log.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
+	}
+}
