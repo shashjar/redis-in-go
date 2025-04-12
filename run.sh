56f983395422c7d1d399b76f4b40ceecf7e68b54
Shashank Jarmale <shashankjarmale1@gmail.com>
2025-04-12 16:39:36 -0400

    Change README & run script back after testing `mygit`

--- run.sh
+++ run.sh
@@ -4,23 +4,9 @@
 
 set -e # Exit early if any commands fail
 
-# Determine the current working directory (the directory that the script is in)
-CURRENT_DIR="$(pwd)"
-
-# Find the project root
-# If we're in the project root, use the current directory
-# Otherwise, we're in a `mygit` repository, so use the parent directory
-if [ "$(basename "$CURRENT_DIR")" = "git-in-go" ]; then
-  PROJECT_ROOT="$CURRENT_DIR"
-else
-  PROJECT_ROOT="$(dirname "$CURRENT_DIR")"
-fi
-
-# Build the Go program in the mygit/ directory
 (
-  cd "$PROJECT_ROOT/mygit"
-  go build -buildvcs="false" -o /tmp/git-in-go .
+  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+  go build -o /tmp/redis-in-go app/*.go
 )
 
-# Run the Go program
-exec /tmp/git-in-go "$@"
+exec /tmp/redis-in-go "$@"
